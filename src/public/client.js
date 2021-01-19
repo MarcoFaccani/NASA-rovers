4b02fb286647296c8733d4f716479f6af52b0072
Marco Faccani <marco.faccani95@gmail.com>
2021-01-19 23:12:27 +0100

    introduced higher-order functions

--- src/public/client.js
+++ src/public/client.js
@@ -13,7 +13,6 @@ const render = async (root, state) => { root.innerHTML = App(state.toJS()) }
 
 //Update App state
 const updateStore = (state, newState) => {
-    console.log(`UPDATING APP STATE`)
     store = state.merge(newState)
     render(root, store)
 }
@@ -30,9 +29,9 @@ window.addEventListener('load', () => {
             if (JSON.stringify(store.toJS().roversPhotos.get(roverName)) === '{}') getRoverPhotos(store, roverName);
             else updateStore(store, {'selectedRover': roverName});
         }
-
     }
 })
+
 
 
 //------------------------------------------------------ create content
@@ -42,80 +41,48 @@ const App = (state) => {
     return `
             <section>
                 ${ImageOfTheDay(apod)}
-                ${RoversDetails(state)}
-                ${ShowRoverPhotos(state)}
+                ${RoversDetails(state, createRoverDetailsCard)}
+                ${ShowRoverPhotos(state, createImagesGrid)}
             </section>
         `
 }
 
 // ------------------------------------------------------  COMPONENTS
 
-const RoversDetails = (state) => {
+
+// Higher-order function
+const RoversDetails = (state, createRoverDetailsCard) => {
     let rovers = state.rovers;
     let roversNames = state.roversNames;
 
-    const dummyRoversImages = ["https://mars.nasa.gov/system/content_pages/main_images/374_mars2020-PIA21635.jpg",
-                                "https://d2pn8kiwq2w21t.cloudfront.net/images/imagesmars202020180921PIA22109-16.width-1320.jpg",
-                                "https://m.dw.com/image/54182462_401.jpg"];
     if (rovers.length === 0 ) getRovers(roversNames);
     else {
         let content = ` <div class="row my-4">
-                        <div class="col mx-auto text-center text-uppercase">
-                            <h1 class="text-white">Choose the drone!</h1>
-                        </div>
-                    </div> `;
-
-    
+                            <div class="col mx-auto text-center text-uppercase">
+                                <h1 class="text-white">Choose the drone!</h1>
+                            </div>
+                        </div> `;
    
         rovers.forEach( (rover, index) => {
-            content = content.concat( `
-                <div class="col-lg-3 col-md-6 mx-auto my-5">
-                    <div class="card text-center m-2">
-                        <img src="${dummyRoversImages[index]}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">${rover.name}</h5>
-                            <ul class="list-group list-group-flush">
-                                <li class="list-group-item">Status: ${rover.status}</li>
-                                <li class="list-group-item">Launch date: ${rover.launch_date}</li>
-                                <li class="list-group-item">Landing date: ${rover.landing_date}</li>
-                                <li class="list-group-item">Most recent photo: ${rover.photos.map(photoObj => photoObj.earth_date).reduce( (date, currentDate) => currentDate > date ? currentDate : date )}</li>
-                                <li class="list-group-item">Total photos: ${rover.total_photos}</li>
-                            </ul>
-                            <a href="#" class="btn btn-primary mt-3">Show me!</a>
-                        </div>
-                    </div>
-                </div> `
-            )
+            content = content.concat(createRoverDetailsCard(rover, index))
         })
 
-        return `<div class="row py-5">
-                    ${content}
-                </div>
-                `
+        return `<div class="row py-5"> ${content} </div> `;
     }
     
 }
 
-// Show rover photos and details
-const ShowRoverPhotos = (state, roverName) => {
+// Show rover photos and details | Higher-order Function
+const ShowRoverPhotos = (state, createImagesGrid) => {
     
     if (state.selectedRover != '' && state.selectedRover != undefined) {
-        console.log('SHOW PHOTOS')
-        let content = ``;
-        
-        state.roversPhotos.get(state.selectedRover).forEach( photo => {
-            content = content.concat(`<div class="col-lg-3 col-md-5 col-sm-10 mx-auto rounded m-4">
-                                            <img src="${photo}" class="img-fluid">
-                                        </div>`);
-        })
         
         return `<div class="container">
                     <div class="row py-5 text-center">
                         <h2 class="text-white text-uppercase mb-5">${state.selectedRover}'s Photos</h2>
-                        ${content}
+                        ${createImagesGrid(state)}
                     </div>
-                </div>
-                `
+                </div> `
     }
    
 }
@@ -154,10 +121,49 @@ const ImageOfTheDay = (apod) => {
     }
 }
 
-// ------------------------------------------------------  API CALLS
+
+
+// ------------------------------------------------------  API CALLS & Utility methods
+
+const createRoverDetailsCard = (rover, index) => {
+
+    const dummyRoversImages = ["https://mars.nasa.gov/system/content_pages/main_images/374_mars2020-PIA21635.jpg",
+                                "https://d2pn8kiwq2w21t.cloudfront.net/images/imagesmars202020180921PIA22109-16.width-1320.jpg",
+                                "https://m.dw.com/image/54182462_401.jpg"];
+
+    return  `
+    <div class="col-lg-3 col-md-6 mx-auto my-5">
+        <div class="card text-center m-2">
+            <img src="${dummyRoversImages[index]}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${rover.name}</h5>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Status: ${rover.status}</li>
+                    <li class="list-group-item">Launch date: ${rover.launch_date}</li>
+                    <li class="list-group-item">Landing date: ${rover.landing_date}</li>
+                    <li class="list-group-item">Most recent photo: ${rover.photos.map(photoObj => photoObj.earth_date).reduce( (date, currentDate) => currentDate > date ? currentDate : date )}</li>
+                    <li class="list-group-item">Total photos: ${rover.total_photos}</li>
+                </ul>
+                <a href="#" class="btn btn-primary mt-3">Show me!</a>
+            </div>
+        </div>
+    </div> `
+
+}
+
+const createImagesGrid = (state) => {
+    let content = ``;
+        
+    state.roversPhotos.get(state.selectedRover).forEach( photo => {
+        content = content.concat(`<div class="col-lg-3 col-md-5 col-sm-10 mx-auto rounded m-4">
+                                        <img src="${photo}" class="img-fluid">
+                                    </div>`);
+    })
+
+    return content;
+}
 
 const getImageOfTheDay = () => {
-    console.log('getImageOfTheDay')
     fetch(`http://localhost:3000/apod`)
         .then(res => res.json())
         .then(apod => updateStore(store, Immutable.Map( { apod } ) ) )
@@ -165,15 +171,12 @@ const getImageOfTheDay = () => {
 
 
 const getRovers = (roversNames) => {
-    console.log('getRovers');
-
     const urls = Array.from(roversNames).map(roverName => `http://localhost:3000/rover?name=${roverName}`);
 
     Promise.all(urls.map(url => 
         fetch(url).then(res => res.json())
     )).then( data => {
         const rovers = data.map( rover => rover.rover.photo_manifest)
-        console.log(rovers)
         updateStore(store, {rovers: rovers})
     })
 
@@ -181,7 +184,6 @@ const getRovers = (roversNames) => {
 
 
 const getRoverPhotos = (state, roverName) => {
-    console.log(`getRoverPhotos - roverName: ${roverName}`);
     const roversPhotos = state.toJS().roversPhotos;
 
     fetch(`http://localhost:3000/rover-photos?name=${roverName}`)
